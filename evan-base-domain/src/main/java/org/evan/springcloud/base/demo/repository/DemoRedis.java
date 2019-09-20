1e5ed9c21e4d62393a3ce25acb3a8c23c54f3de5
沈炜 <277469513@qq.com>
2019-09-20 17:00:24 +0800

    demo

--- evan-base-domain/src/main/java/org/evan/springcloud/base/demo/repository/DemoRedis.java
+++ evan-base-domain/src/main/java/org/evan/springcloud/base/demo/repository/DemoRedis.java
@@ -4,5 +4,5 @@ package org.evan.springcloud.base.demo.repository;
  * @author Evan.Shen
  * @since 2019-09-19
  */
-public class DemoCache {
+public class DemoRedis {
 }
---
+++ evan-base-domain/src/main/java/org/evan/springcloud/base/demo/repository/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author Evan.Shen
+ * @since 2019-08-09
+ *
+ * 数据存储package
+ */
+package org.evan.springcloud.base.demo.repository;
